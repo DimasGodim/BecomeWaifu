5e704d94b9e1bb85fbbdeac79ea1607ad6c50395
DimasGodim <99389617+DimasGodim@users.noreply.github.com>
2023-07-27 09:13:44 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,7 +2,7 @@ import os
 from pydantic import BaseSettings, Field
 
 class Config(BaseSettings):
-    api_key_voicevox: str = Field("u9J87-E5l-03809")
-    url_database:  str = Field("mysql://root:230205@localhost:3306/data")
+    api_key_voicevox: str = Field("")
+    url_database:  str = Field("")
 
 config = Config()
