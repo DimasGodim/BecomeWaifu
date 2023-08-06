782652185431cea210973294ecd4079310334205
DimasGodim <99389617+DimasGodim@users.noreply.github.com>
2023-08-07 05:52:12 +0700

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,8 @@ from tortoise.models import Model
 from tortoise import fields
 
 class logaudio(Model):
-    id = fields.UUIDField(pk=True)
+    audio_id = fields.UUIDField(pk=True)
+    user_id = fields.CharField(max_length=225)
     transcript = fields.CharField(max_length=225)
     translate = fields.CharField(max_length=225)
     audio_file = fields.BinaryField()
@@ -14,7 +15,7 @@ class logaudio(Model):
         return self.id
 
 class userdata(Model):
-    id = fields.UUIDField(pk=True)
+    user_id = fields.UUIDField(pk=True)
     nama = fields.CharField(max_length=225)
     email = fields.CharField(max_length=225)
 
