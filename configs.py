16156d00d9f0bbdb933f583214d5fc90afb09ba4
DimasGodim <99389617+DimasGodim@users.noreply.github.com>
2023-08-08 11:08:23 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,10 +2,10 @@ import os
 from pydantic import BaseSettings, Field
 
 class Config(BaseSettings):
-    api_key_voicevox: str = Field("I55906n808M254E")
-    url_database:  str = Field("mysql://root:123456@localhost:3306/data")
-    redirect_uri_register: str = Field("https://ad46-180-254-85-187.ngrok-free.app/auth2callbackRegister")
-    redirect_uri_login: str = Field("https://ad46-180-254-85-187.ngrok-free.app/auth2callbackLogin")
-    redirect_uri_page_masuk: str = Field("https://ad46-180-254-85-187.ngrok-free.app")
+    api_key_voicevox: str = Field("")
+    url_database:  str = Field("")
+    redirect_uri_register: str = Field("")
+    redirect_uri_login: str = Field("")
+    redirect_uri_page_masuk: str = Field("")
 
 config = Config()
